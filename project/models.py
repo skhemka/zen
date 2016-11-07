76cde763d2bd17dbfb418fd45c022c4b4b14263e
Shreeyagya Khemka <skhemka@andrew.cmu.edu>
2016-11-07 11:45:37 -0500

    Added models other than user

--- project/models.py
+++ project/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from address.models import AddressField
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+#using address from github applicaiton (check back)
+class Location(models.Model):
+    address = AddressField()
+    time_to_campus = models.IntegerField(default=0)
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    duration = models.IntegerField(default=0)
+
+class 
