4016a79be2f87301b9c4b8caeabf349701c3cba4
Shreeyagya Khemka <skhemka@andrew.cmu.edu>
2016-11-28 15:51:13 -0500

    Added models to admin applicaiton

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Category, Location, Consumer, Producer, Event, Preference, Subscription
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Consumer)
+admin.site.register(Producer)
+admin.site.register(Event)
+admin.site.register(Preference)
+admin.site.register(Subscription)
