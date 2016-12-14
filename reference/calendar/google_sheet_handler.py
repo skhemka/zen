e6783e09de4dd104a9e01e7ce05fda65f0d76ca0
Derek Hirata <hirataderek@gmail.com>
2016-12-14 12:53:01 -0500

    Skeleton for interacting with sheets api

---
+++ reference/calendar/google_sheet_handler.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+
+class GoogleSheetHandler:
+
+    def __init__(self, link_read, link_write):
+        self.link_read = link_read
+        self.link_write = link_write
+        self.SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+        self.CLIENT_SECRET_FILE = 'client_secret.json'
+        self.APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+        try:
+            import argparse
+            self.flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+        except ImportError:
+            self.flags = None
+
+    def get_credentials(self):
+        """Gets valid user credentials from storage.
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'sheets.googleapis.com-python-quickstart.json')
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(self.CLIENT_SECRET_FILE, self.SCOPES)
+            flow.user_agent = self.APPLICATION_NAME
+            if self.flags:
+                credentials = tools.run_flow(flow, store, self.flags)
+            else:  # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
